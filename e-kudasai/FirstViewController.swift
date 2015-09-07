1e02f3511e88c2d0516e6530460d99b273a9db58
Legokichi Duckscallion <legokichi@duxca.com>
2015-09-07 18:09:48 +0900

    Initial Commit

---
+++ e-kudasai/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  e-kudasai
+//
+//  Created by yohsukeino on 2015/09/07.
+//  Copyright (c) 2015年 duxca. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
